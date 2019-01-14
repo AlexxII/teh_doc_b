89c6095dc17528824c9cbd030cb10688511af017
AlexxII <AlexxII@murmus.ru>
2019-01-14 16:32:55 +0300

    140119_3

---
+++ config/params.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'adminEmail' => 'admin@example.com',
+    'uploadPath' => '/var/www/tehdoc/web/upload/tehdoc/tools/',
+    'uploadUrl' => 'http://tehdoc.su/upload/tehdoc/tools/',
+    'sql_details' => [
+        'user' => 'user',
+        'pass' => '16PkjVzk:tk',
+        'db' => 'tehdoc',
+        'host' => '127.0.0.1'
+    ]
+];
