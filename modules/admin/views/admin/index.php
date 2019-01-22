ad6b44582cbe9e14f4005041c399deab609a8f26
AlexxII <murmus51@yandex.ru>
2019-01-22 17:35:18 +0300

    Stable_220119_1732

--- modules/admin/views/admin/index.php
+++ modules/admin/views/admin/index.php
@@ -22,16 +22,7 @@ $about = "Панель администрирования";
 </style>
 
 <div class="admin-pannel">
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-  <h3><?= Html::encode($this->title) . 'TEST' . ?>
-=======
   <h3><?= Html::encode($this->title)?>
->>>>>>> developer
-=======
-  <h3><?= Html::encode($this->title) . 'TEST'?>
->>>>>>> developer
     <sup class="h-title fa fa-question-circle-o" aria-hidden="true"
          data-toggle="tooltip" data-placement="right" title="<?php echo $about ?>"></sup>
   </h3>
