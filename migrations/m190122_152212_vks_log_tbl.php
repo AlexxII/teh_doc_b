d28d9a71257283bd63d6c3ce9353094c2369ba75
AlexxII <murmus51@yandex.ru>
2019-01-22 19:19:30 +0300

    220119_1919

---
+++ migrations/m190122_152212_vks_log_tbl.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m190122_152212_vks_log_tbl
+ */
+class m190122_152212_vks_log_tbl extends Migration
+{
+  const TABLE_NAME = '{{%vks_log_tbl}}';
+
+  public function up()
+  {
+    $tableOptions = null;
+    if ($this->db->driverName === 'mysql') {
+      $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+    }
+    $this->createTable(self::TABLE_NAME, [
+      'id' => $this->bigPrimaryKey(),
+      'session_id' => $this->integer(),
+      'log_text' => $this->string(255),
+      'log_time' => $this->date(),
+      'valid' => $this->boolean()->defaultValue(1),
+    ], $tableOptions);
+  }
+
+  public function down()
+  {
+    $this->dropTable(self::TABLE_NAME);
+    return false;
+  }
+}
