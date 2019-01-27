12f9f0a98ee171c30788c8966d82dada88183d3c
AlexxII <murmus51@yandex.ru>
2019-01-27 17:49:29 +0300

    270119_1749_4

--- migrations/m190127_143113_alter_vks_sessions_tbl.php
+++ migrations/m190127_143113_alter_vks_sessions_tbl.php
@@ -9,18 +9,9 @@ class m190127_143113_alter_vks_sessions_tbl extends Migration
 {
   const TABLE_NAME = '{{%vks_sessions_tbl}}';
 
-  /**
-   * {@inheritdoc}
-   */
   public function safeUp()
   {
+    $this->addColumn(self::TABLE_NAME, 'combined', $this->boolean()->defaultValue(false));
     $this->alterColumn(self::TABLE_NAME, 'vks_cancel', $this->boolean()->defaultValue(false));
-  }
-  /**
-   * {@inheritdoc}
-   */
-  public function safeDown()
-  {
-    $this->dropColumn(self::TABLE_NAME, 'combined');
   }
 }
