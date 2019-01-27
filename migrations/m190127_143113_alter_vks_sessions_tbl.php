b0c2bf6db318ddeee5af2b7ecb12e9d31cc43697
AlexxII <murmus51@yandex.ru>
2019-01-27 17:41:21 +0300

    vks updates 270119_1657_2

--- migrations/m190127_143113_alter_vks_sessions_tbl.php
+++ migrations/m190127_143113_alter_vks_sessions_tbl.php
@@ -3,9 +3,9 @@
 use yii\db\Migration;
 
 /**
- * Class m190126_161222_alter_vks_sessions_tbl
+ * Class m190127_143113_alter_vks_sessions_tbl
  */
-class m190126_161222_alter_vks_sessions_tbl extends Migration
+class m190127_143113_alter_vks_sessions_tbl extends Migration
 {
   const TABLE_NAME = '{{%vks_sessions_tbl}}';
 
@@ -14,7 +14,7 @@ class m190126_161222_alter_vks_sessions_tbl extends Migration
    */
   public function safeUp()
   {
-    $this->addColumn(self::TABLE_NAME, 'combined', $this->boolean()->defaultValue(false));
+    $this->alterColumn(self::TABLE_NAME, 'vks_cancel', $this->boolean()->defaultValue(false));
   }
   /**
    * {@inheritdoc}
