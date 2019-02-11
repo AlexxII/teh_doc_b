54d2812c088d56ec1e688ce4c845283c119b22c6
AlexxII <admin@local.com>
2019-02-11 10:51:18 +0300

    Tehdoc: complex upgrade

--- config/params.php
+++ config/params.php
@@ -2,13 +2,10 @@
 
 return [
     'adminEmail' => 'admin@example.com',
-<<<<<<< HEAD
     'uploadPath' => '/var/www/develop.tehdoc/upload/tehdoc/tools/',
     'uploadUrl' => 'http://tehdoc.ru/upload/tehdoc/tools/',
-=======
     'uploadPath' => '/var/www/develop.tehdoc/web/upload/tehdoc/tools/',
     'uploadUrl' => 'http://dev.tehdoc.ru/upload/tehdoc/tools/',
->>>>>>> origin/developer
     'sql_details' => [
         'user' => 'tehuser',
         'pass' => '65<tcVeh',
