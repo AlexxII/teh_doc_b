c118268f3883c631418856254d1942f70f4577d1
AlexxII <admin@sever-fo.ru>
2019-04-01 17:54:36 +0300

    -w TehDoc create

--- config_template/db.php
+++ config_template/db.php
@@ -2,13 +2,16 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=',
-    'username' => 'user',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=tehdoc',
+    'username' => 'tehuser',
+    'password' => '65<tcVeh',
     'charset' => 'utf8',
     'enableSchemaCache' => true,
   // Продолжительность кеширования схемы.
     'schemaCacheDuration' => 3600,
   // Название компонента кеша, используемого для хранения информации о схеме
     'schemaCache' => 'cache',
+    'attributes' => [
+      PDO::MYSQL_ATTR_INIT_COMMAND => "SET sql_mode=(SELECT REPLACE(@@sql_mode, 'ONLY_FULL_GROUP_BY',''))"
+    ]
 ];
