ebfa5623ab2193f3c77fedaa65001c04c63d248b
AlexxII <murmus51@yandex.ru>
2019-05-21 00:25:47 +0300

    -h TehDoc schedule update

--- modules/scheduler/views/default/default.php
+++ modules/scheduler/views/default/default.php
@@ -16,15 +16,60 @@ AirDatepickerAsset::register($this);
 
 ?>
 <style>
+  html, body {
+    margin: 0;
+    padding: 0;
+    font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
+    font-size: 14px;
+  }
 
+  #external-events {
+    width: 150px;
+    padding: 10px 10px;
+    border: 1px solid #ccc;
+    background: #eee;
+  }
+
+  .demo-topbar + #external-events { /* will get stripped out */
+    top: 60px;
+  }
+
+  #external-events .fc-event {
+    margin: 1em 0;
+    cursor: move;
+  }
+
+  #calendar-container {
+    position: relative;
+    z-index: 1;
+    margin-left: 200px;
+  }
+
+  #calendar {
+    max-width: 900px;
+    margin: 20px auto;
+  }
 
 </style>
-
 
 
 <div class="row">
   <div class="col-md-3 col-lg-3" style="margin-bottom: 15px">
     <div id="nav-calendar"></div>
+    <div id='external-events'>
+      <p>
+        <strong>Draggable Events</strong>
+      </p>
+      <div class='fc-event'>My Event 1</div>
+      <div class='fc-event'>My Event 2</div>
+      <div class='fc-event'>My Event 3</div>
+      <div class='fc-event'>My Event 4</div>
+      <div class='fc-event'>My Event 5</div>
+      <p>
+        <input type='checkbox' id='drop-remove'/>
+        <label for='drop-remove'>remove after drop</label>
+      </p>
+    </div>
   </div>
   <div class="col-md-9 col-lg-9">
     <div id="calendar">
@@ -35,12 +80,28 @@ AirDatepickerAsset::register($this);
 
 <script>
 
-  var calendar;
+  var calendar, Draggable;
 
   $(document).ready(function () {
+
+    calendar = FullCalendar.Calendar;
+    Draggable = FullCalendarInteraction.Draggable;
+
+    var containerEl = document.getElementById('external-events');
+    // initialize the external events
+    // -----------------------------------------------------------------
+    new Draggable(containerEl, {
+      itemSelector: '.fc-event',
+      eventData: function (eventEl) {
+        return {
+          title: eventEl.innerText
+        };
+      }
+    });
+
     var calendarEl = document.getElementById('calendar');
     calendar = new FullCalendar.Calendar(calendarEl, {
-      plugins: ['dayGrid', 'timeGrid', 'interaction', 'bootstrap'],
+      plugins: ['interaction', 'dayGrid', 'timeGrid', 'bootstrap'],
       locale: 'ru',
       themeSystem: 'bootstrap',
       weekNumbers: true,
@@ -51,16 +112,26 @@ AirDatepickerAsset::register($this);
         // console.log(info.dateStr);
         // info.dayEl.style.backgroundColor = 'red';
       },
+      droppable: true, // this allows things to be dropped onto the calendar
+      drop: function (info) {
+      },
       buttonText: {
-        month:    'M',
-        week:     'Н',
-        day:      'Д',
-        list:     'Лист'
+        month: 'M',
+        week: 'Н',
+        day: 'Д',
+        list: 'Лист'
+      },
+      bootstrapFontAwesome: {
+        close: 'fa-times',
+        prev: 'fa-chevron-left',
+        next: 'fa-chevron-right',
+        prevYear: 'fa-angle-double-left',
+        nextYear: 'fa-angle-double-right'
       },
       header: {
         left: 'dayGridMonth,timeGridWeek,timeGridDay',
         center: 'title',
-        right: 'today',
+        right: 'today prev,next',
       },
       customButtons: {
         custom1: {
