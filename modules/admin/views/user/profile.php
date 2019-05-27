5fad4264e759956bce52619ba2190cba4f165742
AlexxII <murmus51@yandex.ru>
2019-05-28 00:54:03 +0300

    -h TehDoc profile update

--- modules/admin/views/user/profile.php
+++ modules/admin/views/user/profile.php
@@ -36,7 +36,7 @@ $this->params['breadcrumbs'][] = $this->title;
       <?php echo Yii::$app->user->identity->login; ?>
     </h3>
     <a class="btn btn-primary btn-sm" role="button" disabled="true">Сменить пароль</a>
-    <a id="holiday_" href="calendar-form" class="btn btn-primary btn-sm" role="button">График</a>
+    <a id="holiday" href="calendar-form" class="btn btn-primary btn-sm" role="button">График</a>
     <a id="holiday_" type="button" onclick="notifSet ()" class="btn btn-primary btn-sm">Уведомления</a>
   </div>
 </div>
@@ -44,65 +44,70 @@ $this->params['breadcrumbs'][] = $this->title;
 
 <script>
 
-    $(document).ready(function () {
-        $('[data-toggle="tooltip"]').tooltip();
+  $(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
 
-        $('#holiday').on('click', function (e) {
-            e.preventDefault();
-            $.confirm({
-                content: function () {
-                    var self = this;
-                    self.setContent('Checking callback flow');
-                    return $.ajax({
-                        url: 'calendar-form',
-                        method: 'get'
-                    }).done(function (response) {
-                        self.setContentAppend('<div>' + response + '</div>');
-                    }).fail(function () {
-                        self.setContentAppend('<div>Fail!</div>');
-                    }).always(function () {
-                        self.setContentAppend('<div>Always!</div>');
-                    });
-                },
-                contentLoaded: function (data, status, xhr) {
-                    this.setContentAppend('<div>Content loaded!</div>');
-                },
-                onContentReady: function () {
-                    this.setContentAppend('<div>1</div>');
-                }
-            });
-        });
-
-        $('.input-daterange input').each(function () {
-            $(this).datepicker('clearDates');
-        });
-
+    $('#holiday').on('click', function (e) {
+      e.preventDefault();
+      $.confirm({
+        title: 'График отпусков',
+        type: 'blue',
+        buttons: {
+          go: {
+            btnClass: 'btn-success',
+            text: 'Сохранить',
+            action: function () {
+            }
+          },
+          cancel: {
+            text: 'Отмена'
+          }
+        },
+        content: function () {
+          var self = this;
+          return $.ajax({
+            url: 'calendar-form',
+            method: 'get'
+          }).done(function (response) {
+            self.setContentAppend(response);
+            // self.setContentAppend('<div>' + response + '</div>');
+          }).fail(function () {
+            self.setContentAppend('<div>Fail!</div>');
+          });
+        },
+        contentLoaded: function (data, status, xhr) {
+          // this.setContentAppend('<div>Content loaded!</div>');
+        },
+      });
     });
 
+    $('.input-daterange input').each(function () {
+      $(this).datepicker('clearDates');
+    });
 
-    function notifyMe() {
-        var notification = new Notification('Все еще отбеливаете??', {
-            tag: 'qwe',
-            body: 'Тогда мы идем к Вам!',
-            renotify: true
-        });
+  });
+
+  function notifyMe() {
+    var notification = new Notification('Все еще отбеливаете??', {
+      tag: 'qwe',
+      body: 'Тогда мы идем к Вам!'
+    });
+  }
+
+  function notifSet() {
+    if (!('Notification' in window))
+      console.log('Браузер не поддерживает уведомления!!');
+    else if (Notification.permission === 'granted')
+      setTimeout(notifyMe, 5000);
+    else if (Notification.permission !== 'denied') {
+      Notification.requestPermission(function (permission) {
+        if (!('permission' in Notification))
+          Notification.permission = permission;
+        if (permission === 'granted')
+          setTimeout(notifyMe, 5000);
+      })
     }
-
-
-    function notifSet() {
-        if (!('Notification' in window))
-            console.log('Браузер не поддерживает уведомления!!');
-        else if (Notification.permission === 'granted')
-            setTimeout(notifyMe, 20000);
-        else if (Notification.permission !== 'denied') {
-            Notification.requestPermission(function (permission) {
-                if (!('permission' in Notification))
-                    Notification.permission = permission;
-                if (permission === 'granted')
-                    setTimeout(notifyMe, 20000);
-            })
-        }
-    }
+  }
 
 
 </script>
