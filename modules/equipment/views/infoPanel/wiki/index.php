6597bed4cbe5001f49f3ebfe85315853cdc59334
AlexxII <murmus51@yandex.ru>
2019-07-28 14:00:06 +0300

    -h riac update

--- modules/equipment/views/infoPanel/wiki/index.php
+++ modules/equipment/views/infoPanel/wiki/index.php
@@ -5,19 +5,10 @@ use yii\helpers\Html;
 
 ?>
 
-<style>
-  .panel-body {
-    padding: 5px;
-  }
-  .d-block {
-    text-decoration: #0b3e6f;
-  }
-</style>
-
-<div class="complex-wiki-update" style="margin-top: 15px">
+<div class="complex-wiki-update">
   <div class="row">
     <div class="col-lg-9 col-md-6">
-      <h3 style="margin-top: 0px"><?= Html::encode($model->wiki_title) ?></h3>
+      <h3><?= Html::encode($model->wiki_title) ?></h3>
     </div>
     <div class="col-lg-3 col-md-6 text-right">
       <p>
@@ -29,12 +20,12 @@ use yii\helpers\Html;
 
   <div class="complex-wiki">
     <div class="wiki-content">
-      <div id="wiki-body" class="col-lg-9 col-md-9" style="padding-left:0px">
+      <div id="wiki-body" class="col-lg-9 col-md-9">
         <?= Markdown::process($model->wiki_text, 'extra') ?>
       </div>
-      <div id="wiki-rightbar" class="col-lg-3 col-md-3 text-right" style="padding: 0px">
+      <div id="wiki-rightbar" class="col-lg-3 col-md-3 text-right">
         <div class="panel panel-default">
-          <div class="panel-heading" style="text-align: left">
+          <div class="panel-heading">
             Страницы
           </div>
           <ul class="list-group">
