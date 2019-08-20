87e87e6a69d668e77d56e0e68756e72c3169c7e0
AlexxII <murmus51@yandex.ru>
2019-08-20 11:42:17 +0300

    -h riac update

--- modules/scheduler/views/default/index.php
+++ modules/scheduler/views/default/index.php
@@ -274,7 +274,9 @@ BootstrapYearCalendarAsset::register($this);
               },
               do: {
                 btnClass: 'btn-green',
-                text: 'OK',
+                text: '<svg width="24" height="24" viewBox="0 0 45 45" style="fill:#fff">'+
+                  '<path d="M1.36,17.427c0,0,7.311-0.122,10.844,8.163c0,0,15.474-22.175,31.435-18.885c0,0-17.789,7.067-32.045,31.922L1.36,17.427z"/>'+
+                  '</svg>',
                 action: function () {
                     console.log('Событие исполнено!');
                 }
