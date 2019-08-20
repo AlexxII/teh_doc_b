be769136812ed301640521225cce99dc431f0304
AlexxII <admin@local.com>
2019-08-20 17:25:14 +0300

    -w riac update

--- modules/scheduler/views/menu/left_menu.php
+++ modules/scheduler/views/menu/left_menu.php
@@ -1,5 +1,31 @@
 <div id="left-menu">
   <div class="menu-list">
-
+    <div id="datepicker" style="margin-left: 30px"></div>
   </div>
+  <div>
+    <p style="padding-left: 30px">Добавить календарь</p>
+  </div>
+  <?php
+/*  TODO Таблица с календарями:
+  - id календаря
+  - title наименование календаря
+  - user_id пользователь, который создал
+  - created дата создания
+  - color цвет календаря
+
+
+*/
+
+?>
 </div>
+
+<script>
+    $('#datepicker').datepicker({
+        language: 'ru',
+        todayHighlight: true
+    }).on('changeDate', function (info) {
+        console.log(info.date);
+        calendar.gotoDate(info.date)
+    });
+</script>
+
