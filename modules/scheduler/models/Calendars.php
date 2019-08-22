a7a58c61589ed70247b794fb73c738c4ab34ffb1
AlexxII <admin@local.com>
2019-08-22 17:55:44 +0300

    -w riac update

---
+++ modules/scheduler/models/Calendars.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace app\modules\scheduler\models;
+
+use app\base\MHelper;
+
+
+class Calendars extends \yii\db\ActiveRecord
+{
+  public static function tableName()
+  {
+    return 'calendars_pull_tbl';
+  }
+
+  public function __construct()
+  {
+    $this->id = MHelper::generateId();
+  }
+
+  public function attributeLabels()
+  {
+    return [
+      'id' => 'ID',
+      'title' => 'Наименование:',
+      'description' => 'Описание:',
+      'color' => 'Цвет:'
+    ];
+  }
+
+  public function rules()
+  {
+    return [
+      [['title'], 'required'],
+      [['description'], 'string'],
+    ];
+  }
+
+}
