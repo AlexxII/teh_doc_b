c1bdf164e9a291e0999bc469d3f670e970a39f75
AlexxII <murmus51@yandex.ru>
2019-08-23 11:59:46 +0300

    -h riac update

--- modules/scheduler/views/default/index.php
+++ modules/scheduler/views/default/index.php
@@ -112,6 +112,7 @@ BootstrapYearCalendarAsset::register($this);
       '<path d="M14.06 9.02l.92.92L5.92 19H5v-.92l9.06-9.06M17.66 3c-.25 0-.51.1-.7.29l-1.83 1.83 3.75 3.75' +
       '1.83-1.83c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.2-.2-.45-.29-.71-.29zm-3.6 3.19L3 17.25V21h3.75L17.81 9.94l-3.75-3.75z"></path>' +
       '</svg></div>';
+
     var deleteBtn = '<div id="event-delete-btn" title="Удалить к чертям" onClick="deleteEvent()">' +
       '<svg width="20" height="20" viewBox="2 2 22 22">' +
       '<path d="M15 4V3H9v1H4v2h1v13c0 1.1.9 2 2 2h10c1.1 0 2-.9 2-2V6h1V4h-5zm2 15H7V6h10v13z"></path>' +
