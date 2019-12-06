18ce4529f3289f014ac4cb18a2fee535e55e7ab3
AlexxII <admin@local.com>
2019-12-06 14:24:32 +0300

    -w riac update

--- modules/opros/views/default/index.php
+++ modules/opros/views/default/index.php
@@ -3,20 +3,24 @@
     <label for="fileToUpload">Зашрузить XML файл</label><br/>
     <input type="file" name="oprosxml" id="xmlupload"/>
   </div>
+  <div id="">
+
+  </div>
 </div>
 
 <script>
 
   $(document).ready(function (e) {
-    $('#xmlupload').on('change', function () {
-      event.preventDefault();
+    $('#xmlupload').on('change', function (e) {
+      e.preventDefault();
       var selectedFile = document.getElementById('xmlupload').files[0];
       var reader = new FileReader();
       reader.onload = function (e) {
         readXml = e.target.result;
+        console.log(readXml);
         var parser = new DOMParser();
         var doc = parser.parseFromString(readXml, "application/xml");
-        x = doc.getElementsByTagName("ARTIST");
+        x = doc.getElementsByTagName("vopros");
         console.log(x);
       };
       reader.readAsText(selectedFile);
