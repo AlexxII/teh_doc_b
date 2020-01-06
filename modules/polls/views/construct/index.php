6ae651333b094b0a5ba6dc85b3c7686cae95b369
AlexxII <murmus51@yandex.ru>
2020-01-06 12:56:02 +0300

    -h riac update

--- modules/polls/views/construct/index.php
+++ modules/polls/views/construct/index.php
@@ -247,10 +247,57 @@
       <div class="dde-menu-item">Удалить вопрос</div>
     </div>
   </div>
+
+  <div class="panel panel-default">
+    <div class="panel-body">
+      <div class="btn-group">
+        <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+          <span data-bind="label">Select One</span>&nbsp;<span class="caret"></span>
+        </button>
+        <ul class="dropdown-menu" role="menu">
+          <li><a href="#">Item 1</a></li>
+          <li><a href="#">Another item</a></li>
+          <li><a href="#">This is a longer item that will not fit properly</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </div>
 
 
 <script>
+/*
+  (function () {
+    // hold onto the drop down menu
+    var dropdownMenu;
+
+    // and when you show it, move it to the body
+    $(window).on('show.bs.dropdown', function (e) {
+
+      // grab the menu
+      dropdownMenu = $(e.target).find('.dropdown-menu');
+
+      // detach it and append it to the body
+      $('body').append(dropdownMenu.detach());
+
+      // grab the new offset position
+      var eOffset = $(e.target).offset();
+
+      // make sure to place it where it would normally go (this could be improved)
+      dropdownMenu.css({
+        'display': 'block',
+        'top': eOffset.top + $(e.target).outerHeight(),
+        'left': eOffset.left
+      });
+    });
+
+    // and when you hide it, reattach the drop down, and hide it normally
+    $(window).on('hide.bs.dropdown', function (e) {
+      $(e.target).append(dropdownMenu.detach());
+      dropdownMenu.hide();
+    });
+  })();
+*/
 
   let dDeFlag = false;
 
