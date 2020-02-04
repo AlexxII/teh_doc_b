899bd38aabf73e66514a57fce8aa1ad06efad46f
AlexxII <admin@local.com>
2020-02-04 16:55:44 +0300

    -w riac update

--- modules/polls/lib/js/question.class.js
+++ modules/polls/lib/js/question.class.js
@@ -45,7 +45,7 @@ class Question {
     selectTemplate.appendChild(select);
     let selectData = this.prepareData();
     selectData.forEach(function (element, key) {
-      select[key + 1] = new Option (element.text, element.id);
+      select[key + 1] = new Option(element.text, element.id);
     });
     this.selectObj = select;
     return selectTemplate;
@@ -99,6 +99,37 @@ function Answer(config) {
   this.unique = +config.unique;
   this.type = +config.input_type;
 
+  this.renderUniqueSymbl = function () {
+    let uniqueNode = document.createElement('span');
+    uniqueNode.className = 'drive-unique-answer';
+    let uniqueSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    uniqueSvg.setAttribute('width', 15);
+    uniqueSvg.setAttribute('height', 15);
+    uniqueSvg.setAttribute('viewBox', '0 0 560.317 560.316');
+    let path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    path.setAttributeNS(null, 'd', 'M 207.523 560.316 c 0 0 194.42 -421.925 194.444 -421.986 l 10.79 -23.997 c -41.824 12.02 -135.271' +
+      '34.902 -135.57 35.833 C 286.96 122.816 329.017 0 330.829 0 c -39.976 0 -79.952 0 -119.927 0 l -12.167 57.938' +
+      'l -51.176 209.995 l 135.191 -36.806 L 207.523 560.316 Z');
+    uniqueSvg.appendChild(path);
+    uniqueNode.appendChild(uniqueSvg);
+    this.uniqueSymbl = uniqueNode;
+  };
+
+  this.renderFreeSymbl = function () {
+    let freeNode = document.createElement('span');
+    freeNode.className = 'drive-free-answer';
+    let editSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    editSvg.setAttribute('width', 15);
+    editSvg.setAttribute('height', 15);
+    editSvg.setAttribute('viewBox', '0 0 20 20');
+    let path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    path.setAttributeNS(null, 'd', 'M16.77 8l1.94-2a1 1 0 0 0 0-1.41l-3.34-3.3a1 1 0 0 0-1.41 0L12 3.23zm-5.81-3.71L1 ' +
+      '14.25V19h4.75l9.96-9.96-4.75-4.75z');
+    editSvg.appendChild(path);
+    freeNode.appendChild(editSvg);
+    this.freeSymbl = freeNode;
+  };
+
   this.renderAnswer = function (index) {
     let answerTemplate = document.createElement('p');
     let strong = document.createElement('strong');
@@ -111,37 +142,11 @@ function Answer(config) {
     answerTemplate.appendChild(strong);
     let titleNode = document.createTextNode(this.title);
     answerTemplate.append(titleNode);
-
     if (this.type === TYPE_FREE_ANSWER) {
-      let freeNode = document.createElement('span');
-      freeNode.className = 'drive-free-answer';
-      let editSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-      editSvg.setAttribute('width', 15);
-      editSvg.setAttribute('height', 15);
-      editSvg.setAttribute('viewBox', '0 0 20 20');
-      let path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-      path.setAttributeNS(null, 'd', 'M16.77 8l1.94-2a1 1 0 0 0 0-1.41l-3.34-3.3a1 1 0 0 0-1.41 0L12 3.23zm-5.81-3.71L1 ' +
-        '14.25V19h4.75l9.96-9.96-4.75-4.75z');
-      editSvg.appendChild(path);
-      freeNode.appendChild(editSvg);
-      answerTemplate.appendChild(freeNode);
+      answerTemplate.appendChild(this.freeSymbl);
     }
-
-    if (this.unique == 1) {
-      let uniqueNode = document.createElement('span');
-      uniqueNode.className = 'drive-unique-answer';
-      let uniqueSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-      uniqueSvg.setAttribute('width', 15);
-      uniqueSvg.setAttribute('height', 15);
-      uniqueSvg.setAttribute('viewBox', '0 0 560.317 560.316');
-      let path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-      path.setAttributeNS(null, 'd', 'M 207.523 560.316 c 0 0 194.42 -421.925 194.444 -421.986 l 10.79 -23.997 c -41.824 12.02 -135.271' +
-        '34.902 -135.57 35.833 C 286.96 122.816 329.017 0 330.829 0 c -39.976 0 -79.952 0 -119.927 0 l -12.167 57.938' +
-        'l -51.176 209.995 l 135.191 -36.806 L 207.523 560.316 Z');
-      uniqueSvg.appendChild(path);
-      uniqueNode.appendChild(uniqueSvg);
-      answerTemplate.appendChild(uniqueNode);
-
+    if (this.unique === 1) {
+      answerTemplate.appendChild(this.uniqueSymbl);
     }
     this.visualElement = answerTemplate;
   };
